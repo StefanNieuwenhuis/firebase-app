20cecaecd625794f1398d4a77048e6b2d37203c6
Stefan Nieuwenhuis <stefan.nieuwenhuis@ordina.nl>
2017-05-16 23:06:42 +0200

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
+
 import { AppRoutingModule } from './app-routing.module';
-=======
->>>>>>> b1d973d1cb81e09b21200840e99012e0a3f9e3fb
+
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -15,11 +14,10 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment'
 
 import 'hammerjs';
-<<<<<<< HEAD
+
 
 import {HomeModule} from './home/home.module';
-=======
->>>>>>> b1d973d1cb81e09b21200840e99012e0a3f9e3fb
+
 
 import { AppComponent } from './app.component';
 
@@ -33,17 +31,17 @@ import { AppComponent } from './app.component';
     HttpModule,
     MaterialModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
+
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
-    HomeModule
-=======
+    HomeModule,
+
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule
->>>>>>> b1d973d1cb81e09b21200840e99012e0a3f9e3fb
+
   ],
   providers: [],
   bootstrap: [AppComponent]
