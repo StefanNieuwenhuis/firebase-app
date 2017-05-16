d90c6071b82ac666b761860e1461a6647703bd0d
Stefan Nieuwenhuis <stefan.nieuwenhuis@ordina.nl>
2017-05-16 21:17:25 +0200

    angular-cli, -material and -firebase initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment'
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +21,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBANBG1zaqNUHNtaWd2F3VYWKdoCpPpbjM",
+    authDomain: "fir-test-app-864ef.firebaseapp.com",
+    databaseURL: "https://fir-test-app-864ef.firebaseio.com",
+    projectId: "fir-test-app-864ef",
+    storageBucket: "fir-test-app-864ef.appspot.com",
+    messagingSenderId: "396622601733"
+  }
 };
