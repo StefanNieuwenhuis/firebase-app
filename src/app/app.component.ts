96d58631bc6ef9812a05d287c7aef55cec6ec599
Stefan Nieuwenhuis <stefan.nieuwenhuis@ordina.nl>
2017-05-16 23:09:08 +0200

    routing in place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { Observable } from 'rxjs/Observable';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
-=======
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
->>>>>>> b1d973d1cb81e09b21200840e99012e0a3f9e3fb
+
 
 @Component({
   selector: 'app-root',
@@ -13,27 +7,5 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-<<<<<<< HEAD
-  user: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) {
-    this.user = afAuth.authState;
-  }
-
-  login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-  }
-
-  logout() {
-    this.afAuth.auth.signOut();
-  }
-=======
-  items: FirebaseListObservable<any[]>;
-
-  constructor(db: AngularFireDatabase) {
-    this.items = db.list('/items');
-  }
-
-
->>>>>>> b1d973d1cb81e09b21200840e99012e0a3f9e3fb
 }
